6babcb0111d419b51acdcd35d0ed9ff349d5fbfd
Jay Gibran <jaygibran@gmail.com>
2020-09-07 20:57:34 +0100

    adding send JMS message example

--- src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgjms.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -8,11 +9,13 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 	
+	public static final String MY_QUEUE = "my-hello-world";
+	
+	@Bean
 	public MessageConverter messageConverter() {
 		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
 		converter.setTargetType(MessageType.TEXT);
 		converter.setTypeIdPropertyName("_type");
-		
 		return converter;
 	}
 
