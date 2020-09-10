a49585c4b73faf1196da6acf6781ee5e63be33ad
Jay Gibran <jaygibran@gmail.com>
2020-09-10 19:20:36 +0100

    adding send and receive message example

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -1,8 +1,14 @@
 package guru.springframework.sfgjms.listener;
 
+import java.util.UUID;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 
+import org.springframework.jms.JmsException;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,15 +16,30 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+	
+	private final JmsTemplate jmsTemplate;
 	
 	@JmsListener(destination = JmsConfig.MY_QUEUE)
 	public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) {
 		System.out.println("I Got a Message!");
 		
 		System.out.println(helloWorldMessage);
+	}
+	
+	@JmsListener(destination = JmsConfig.MY_SEND_RC_QUEUE)
+	public void listenForHello(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) throws JmsException, JMSException {
+		HelloWorldMessage payloadMessage = HelloWorldMessage
+				.builder()
+				.id(UUID.randomUUID())
+				.message("World!")
+				.build();
+		
+		jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMessage);
 	}
 
 }
--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -2,9 +2,17 @@ package guru.springframework.sfgjms.sender;
 
 import java.util.UUID;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
@@ -16,9 +24,10 @@ public class HelloSender {
 	
 	private final JmsTemplate jmsTemplate;
 	
-	@Scheduled(fixedRate = 2000)
+	private final ObjectMapper objectMapper;
+	
+	//@Scheduled(fixedRate = 2000)
 	public void sendMessage() {
-		System.out.println("I'm Sending a message");
 		HelloWorldMessage message = HelloWorldMessage
 				.builder()
 				.id(UUID.randomUUID())
@@ -27,7 +36,32 @@ public class HelloSender {
 		
 		jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 		
-		System.out.println("Message Sent!");
+	}
+	
+	@Scheduled(fixedRate = 2000)
+	public void sendAndReceiveMessage() throws JMSException {
+		System.out.println("I'm Sending a message");
+		HelloWorldMessage message = HelloWorldMessage
+				.builder()
+				.id(UUID.randomUUID())
+				.message("Hello!")
+				.build();
+		
+		Message receiveMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RC_QUEUE, new MessageCreator() {
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				try {
+					Message helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+					helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+					System.out.println("Sending Hello");
+					return helloMessage;
+				}  catch (JMSException | JsonProcessingException e) {
+					throw new JMSException("boom");
+				}
+			}
+		});
+		
+		System.out.println(receiveMessage.getBody(String.class));
 		
 	}
 
